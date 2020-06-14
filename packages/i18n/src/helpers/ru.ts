fcb15995e152b7b901838dc468eff5f02f2cf400
Exeteres <exeteres@yandex.ru>
2020-06-14 13:50:05 +0700

    fix(i18n): Fix invalid ru pluralization

--- packages/i18n/src/helpers/ru.ts
+++ packages/i18n/src/helpers/ru.ts
@@ -2,11 +2,12 @@ import { formatPlural } from "@replikit/i18n";
 
 export const ru = {
     plural(value: number, one: string, few: string, many?: string): string {
-        const h = value % 100;
+        const abs = Math.abs(value);
+        const h = abs % 100;
         if (h >= 11 && h <= 20) {
             return formatPlural(many ?? few, value);
         }
-        switch (value % 10) {
+        switch (abs % 10) {
             case 1:
                 return formatPlural(one, value);
             case 2:
--- packages/i18n/tests/helpers/ru.test.ts
+++ packages/i18n/tests/helpers/ru.test.ts
@@ -3,7 +3,7 @@ import { ru } from "@replikit/i18n";
 describe("ru helpers", () => {
     it("should pluralize string", () => {
         const results: string[] = [];
-        for (let i = 1; i <= 30; i++) {
+        for (let i = -30; i <= 30; i++) {
             const result = ru.plural(i, "$ яблоко", "$ яблока", "$ яблок");
             results.push(result);
         }
