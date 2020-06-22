6e6ca9dd77267a198ebd66a019e601c508dabbcb
Exeteres <exeteres@yandex.ru>
2020-06-22 17:43:04 +0700

    fix(permissions): Fix missing storage defaults

--- packages/permissions/src/startup.ts
+++ packages/permissions/src/startup.ts
@@ -1,6 +1,22 @@
-import { applyMixins } from "@replikit/core";
-import { User, Member } from "@replikit/storage";
+import { applyMixins, hook } from "@replikit/core";
+import { User, Member, connection, ConnectionManager } from "@replikit/storage";
 import { HasPermissions } from "@replikit/permissions";
 
 applyMixins(User, [HasPermissions]);
 applyMixins(Member, [HasPermissions]);
+
+export function registerPermissionsDefaults(
+    connection: ConnectionManager
+): void {
+    const users = connection.getRepository(User);
+    users.setDefault("permissions", []);
+    users.setDefault("roles", []);
+
+    const member = connection.getRepository(Member);
+    member.setDefault("permissions", []);
+    member.setDefault("roles", []);
+}
+
+hook("storage:database:done", () => {
+    registerPermissionsDefaults(connection);
+});
