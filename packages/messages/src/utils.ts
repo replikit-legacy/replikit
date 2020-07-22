046ca1b8a19308790f3e0a424a0cb9f3cb7dc7dd
Exeteres <exeteres@yandex.ru>
2020-07-22 20:24:28 +0700

    feat(messages): Add MessageLike type

--- packages/messages/src/utils.ts
+++ packages/messages/src/utils.ts
@@ -1,4 +1,8 @@
 import { createHash } from "crypto";
+import { OutMessage } from "@replikit/core/typings";
+import { MessageBuilder } from "@replikit/messages";
+
+export type MessageLike = OutMessage | MessageBuilder | void;
 
 export function hashString(input: string): string {
     return createHash("sha1")
