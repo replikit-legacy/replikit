ad35bfd186022a468e30546ba76d1e19e262a53b
Exeteres <exeteres@yandex.ru>
2020-07-30 15:47:15 +0700

    example: Add example for inline queries

--- example/modules/random/src/handlers/debug.ts
+++ example/modules/random/src/handlers/debug.ts
@@ -1,18 +1,19 @@
 import { router } from "@replikit/router";
-import { MessageBuilder } from "@replikit/messages";
+import { MessageBuilder, fromCode } from "@replikit/messages";
 import { RandomLocale } from "@example/random";
+import { AttachmentType } from "@replikit/core/src";
 
 router.of("message:edited").use(async context => {
     const locale = context.getLocale(RandomLocale);
     await context.reply(locale.userEditedMessage(context.account));
 });
 
-router.of("account:left").use(async context => {
+router.of("member:left").use(async context => {
     const locale = context.getLocale(RandomLocale);
     await context.reply(locale.accountLeft(context.account));
 });
 
-router.of("account:joined").use(async context => {
+router.of("member:joined").use(async context => {
     const locale = context.getLocale(RandomLocale);
     await context.reply(locale.accountJoined(context.account));
 });
@@ -34,4 +35,26 @@ router.of("channel:photo:edited").use(async context => {
 router.of("channel:photo:deleted").use(async context => {
     const locale = context.getLocale(RandomLocale);
     await context.reply(locale.channelPhotoDeleted);
+});
+
+router.of("inline-query:received").use(async context => {
+    const attachment = {
+        id: "BQADBAADPQIAAl9XmQAB9MwJWKLJogYC",
+        type: AttachmentType.Sticker
+    };
+    await context.answer({
+        results: [
+            { id: "attachment", attachment, message: { text: "жъжъь" } },
+            { id: "text", article: { title: "тест" } },
+            { id: "text1", article: { title: "тест 2", description: "а" } }
+        ],
+        cacheTime: 0
+    });
+});
+
+router.of("inline-query:chosen").use(async context => {
+    await context.controller.sendMessage(
+        context.account.id,
+        fromCode(JSON.stringify(context.result, undefined, 2))
+    );
 });
