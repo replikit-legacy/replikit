d7d46505d18e64d2c27b1e2511c7469361b79167
Exeteres <exeteres@yandex.ru>
2020-08-05 14:05:21 +0700

    chore(authorization): Update locales

--- packages/authorization/src/locales/en.ts
+++ packages/authorization/src/locales/en.ts
@@ -2,5 +2,5 @@ import { locales } from "@replikit/i18n";
 import { AuthorizationLocale } from "@replikit/authorization";
 
 locales.add("en", AuthorizationLocale, {
-    accessDenied: "Access denied"
+    accessDenied: "Access denied!"
 });
--- packages/authorization/src/locales/ru.ts
+++ packages/authorization/src/locales/ru.ts
@@ -2,5 +2,5 @@ import { locales } from "@replikit/i18n";
 import { AuthorizationLocale } from "@replikit/authorization";
 
 locales.add("ru", AuthorizationLocale, {
-    accessDenied: "Доступ запрещен"
+    accessDenied: "Доступ запрещен!"
 });
