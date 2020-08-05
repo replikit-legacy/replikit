48c5836d55294f777a0c6bb8ff605bd57e23e877
Exeteres <exeteres@yandex.ru>
2020-08-05 14:45:15 +0700

    fix(i18n): Allow getLocale method for inline query contexts

--- packages/i18n/typings/extensions/router.d.ts
+++ packages/i18n/typings/extensions/router.d.ts
@@ -1,8 +1,8 @@
 import "@replikit/router/typings";
 import { LocaleConstructor } from "@replikit/i18n/typings";
 
-declare module "@replikit/router/typings/context/channelContext" {
-    export interface ChannelContext {
+declare module "@replikit/router/typings/context/context" {
+    export interface Context {
         getLocale<T>(type: LocaleConstructor<T>): T;
     }
 }
